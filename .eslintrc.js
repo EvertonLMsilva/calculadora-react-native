59d5717b3da3c46ce6649924935908309f7e0eec
Everton Moraes <35983284+EvertonLMsilva@users.noreply.github.com>
2022-09-21 23:43:20 -0300

    feat: add component button (#1)
    Co-authored-by: everton.L.M.silva <everton.silva@adapcon.com.br>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  wrapIife: 1,
+  proseWrap: "always",
+  arrowParens: "avoid",
 };
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Dimensions, Text, StyleSheet, View, TouchableHighlight } from 'react-native';
+import { proseWrap } from '../../.prettierrc';
+
+const style = StyleSheet.create({
+  button: {
+    fontSize: 40,
+    height: Dimensions.get('window').height / 8,
+    width: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+  },
+});
+
+const Button = (props) => {
+    return (
+      <TouchableHighlight onPress={props.onClick}>
+        <Text style={style.button}>{props.label}</Text>
+      </TouchableHighlight>
+    );
+  };
+
+export default Button;
