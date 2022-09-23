4eac05e337716251b1c14f2e2bbe8c97bf225260
everton.L.M.silva <everton.silva@adapcon.com.br>
2022-09-22 21:36:15 -0300

    feat: add display

--- App.js
+++ App.js
@@ -6,33 +6,37 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type { Node } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 const App: () => Node = () => {
+  const [state, setState] = useState({ displayValue: '0' });
+
   return (
     <SafeAreaView style={styles.sectionContainer}>
-      <View style={styles.button}>
-        <Button label="AC" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="*" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
-      </View>
+        <Display value={state.displayValue} />
+        <View style={styles.button}>
+          <Button label="AC" />
+          <Button label="/" />
+          <Button label="7" />
+          <Button label="8" />
+          <Button label="9" />
+          <Button label="*" />
+          <Button label="4" />
+          <Button label="5" />
+          <Button label="6" />
+          <Button label="-" />
+          <Button label="1" />
+          <Button label="2" />
+          <Button label="3" />
+          <Button label="+" />
+          <Button label="0" />
+          <Button label="." />
+          <Button label="=" />
+        </View>
     </SafeAreaView>
   );
 };
@@ -43,8 +47,8 @@ const styles = StyleSheet.create({
   },
   button: {
     flexDirection: 'row',
-    flexWrap: 'wrap'
-  }
+    flexWrap: 'wrap',
+  },
 });
 
 export default App;
