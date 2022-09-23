3a909a15705d9b605e63a5fc9df5e2376cc95a0f
Everton Moraes <35983284+EvertonLMsilva@users.noreply.github.com>
2022-09-22 22:06:15 -0300

    feature/add-functions-at-buttons (#3)
    Co-authored-by: everton.L.M.silva <everton.silva@adapcon.com.br>

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Dimensions, Text, StyleSheet, View, TouchableHighlight } from 'react-native';
+import {
+  Dimensions,
+  Text,
+  StyleSheet,
+  View,
+  TouchableHighlight,
+} from 'react-native';
 
 const style = StyleSheet.create({
   button: {
@@ -12,14 +18,30 @@ const style = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#888',
   },
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
 });
 
-const Button = (props) => {
+const Button = props => {
+  const stylesButton = [style.button];
+
+  if (props.double) stylesButton.push(style.buttonDouble);
+  if (props.triple) stylesButton.push(style.buttonTriple);
+  if (props.operation) stylesButton.push(style.operationButton);
+
     return (
-      <TouchableHighlight onPress={props.onClick}>
-        <Text style={style.button}>{props.label}</Text>
+      <TouchableHighlight onPress={()=>props.onClick(props.label)}>
+        <Text style={stylesButton}>{props.label}</Text>
       </TouchableHighlight>
     );
-  };
+};
 
 export default Button;
